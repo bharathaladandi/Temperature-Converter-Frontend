bca07ad9978fda243483cf6489981f094de6f68e
bharathaladandi <bharathaladandi@gmail.com>
2023-09-08 00:03:12 +0530

    create and handle temp and scale

--- temperature-converter-frontend/src/Components/Homepage.jsx
+++ temperature-converter-frontend/src/Components/Homepage.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
 
 export const Homepage = () => {
+    const [temp, setTemp] = useState('')
+    const [scale, setScale] = useState('')
 
+    const handleChange = (e) => {
+        setTemp(e.target.value)
+        console.log(e.target.value)
+    }
+
+    const handleSelct = (e) => {
+        setScale(e.target.value)
+        console.log(e.target.value)
+    }
     return (
         <div>
             <div>
-                <h1>Todo List</h1>
+                <h1>Temperature Converter</h1>
             </div>
             <div>
-                <input type="number" />
-                <select >
+                <input placeholder='Add Temperature' type="number" value={temp} onChange={handleChange} />
+                <select value={scale} onChange={handleSelct}>
                     <option value="celsius">Celsius</option>
                     <option value="fahrenheit">Fahrenheit</option>
                 </select>
