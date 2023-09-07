1b629a0927e77bd5015ed25a678d0da756ea75b6
bharathaladandi <bharathaladandi@gmail.com>
2023-09-08 00:24:11 +0530

    convert celsius created

--- temperature-converter-frontend/src/Components/Homepage.jsx
+++ temperature-converter-frontend/src/Components/Homepage.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react'
 
 export const Homepage = () => {
-    const [temp, setTemp] = useState('')
-    const [scale, setScale] = useState('')
+    const [temp, setTemp] = useState('');
+    const [scale, setScale] = useState('');
+    const [celsius, SetCelsius] = useState('');
 
     const handleChange = (e) => {
         setTemp(e.target.value)
-        console.log(e.target.value)
+        // console.log(e.target.value)
     }
 
     const handleSelct = (e) => {
         setScale(e.target.value)
-        console.log(e.target.value)
+        // console.log(e.target.value)
+    }
+
+    const handleCelsius = () => {
+        const showCelsius = (temp * 9/5) + 32; 
+        SetCelsius(`${temp}°C is equal to ${showCelsius}°C`)
+        // console.log(`${temp}°C is equal to ${showCelsius}°C`);
     }
     return (
         <div>
@@ -25,6 +32,11 @@ export const Homepage = () => {
                     <option value="fahrenheit">Fahrenheit</option>
                 </select>
             </div>
+            <div>
+                <button onClick={handleCelsius}>Convert to Celsius</button>
+                <button>Convert to Fahrenheit</button>
+            </div>
+            <h3>{celsius}</h3>
         </div>
     )
 }
